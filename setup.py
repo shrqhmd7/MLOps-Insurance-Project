ee20b24baef093c1bfd05da2540b4bc8a71a8db1
Sharique <ahmad_sharique@outlook.com>
2026-01-02 13:55:40 +0530

    Project Structure is ready, virtual environment has been set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sharique Ahmad",
+    author_email="ahmad_sharique@outlook.com",
+    packages=find_packages()
+)
